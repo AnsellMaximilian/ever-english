f82cc3ff74c25339a2dfd87f7871e0ce87259bca
Ansell Maximilian <ansellmaximilian@gmail.com>
2024-04-26 17:07:04 +0700

    end of day

--- constants/exerciseTypes.ts
+++ constants/exerciseTypes.ts
@@ -14,10 +14,21 @@ const exerciseTypes: ExerciseType[] = [
     exampleJSON: "",
   },
   {
-    key: "FILL_IN_THE_BLANKS",
-    name: "Fill in the blanks",
-    description:
-      "User will be given a sentence with missing words. They are to fill in all the blanks with the correct words.",
+    key: "IDENTIFY_PARTS_OF_SPEECH",
+    name: "Identify parts of speech",
+    description: "",
+    exampleJSON: "",
+  },
+  {
+    key: "FORM_SENTENCE",
+    name: "Form sentence",
+    description: "Form a sentence from a list of words",
+    exampleJSON: "",
+  },
+  {
+    key: "MULTIPLE_CHOICE",
+    name: "Multiple choice",
+    description: "",
     exampleJSON: "",
   },
 ];
