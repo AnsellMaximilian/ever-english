825f661af79b221fb653f920cc0eacc4cd9fd78f
Ansell Maximilian <ansellmaximilian@gmail.com>
2024-04-29 09:46:40 +0700

    add types for exercise sessions

--- types/api.ts
+++ types/api.ts
@@ -2,3 +2,49 @@ export type QuizSessionRequestBody = {
   concepts: string[];
   englishLevel: string;
 };
+
+export interface ExerciseSession {
+  exerciseType: string;
+  level: string;
+  concepts: string[];
+}
+
+export interface ConversationExerciseSession extends ExerciseSession {
+  conversations: {
+    concept: string;
+    dialog: {
+      name: string;
+      content: string;
+    }[];
+    choices: {
+      isCorrect: boolean;
+      content: string;
+    }[];
+  }[];
+}
+
+export interface PartsOfSpeechMatchExerciseSession extends ExerciseSession {
+  matchSets: {
+    concept: string;
+    pairs: { word: string; partOfSpeech: string }[];
+  }[];
+}
+
+export interface ComprehensionExerciseSession extends ExerciseSession {
+  texts: {
+    concept: string;
+    text: string;
+    question: string;
+    choices: {
+      answer: string;
+      isCorrect: boolean;
+    }[];
+  }[];
+}
+
+export interface ArrangeSentenceExerciseSession extends ExerciseSession {
+  sentences: {
+    concept: string;
+    sentence: string;
+  }[];
+}
