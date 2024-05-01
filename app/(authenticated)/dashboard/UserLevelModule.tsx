7ac435f7ec7747a76a8db58a840cef5ef1441ee0
Ansell Maximilian <ansellmaximilian@gmail.com>
2024-05-01 07:50:42 +0700

    ui: session dashboard module

--- app/(authenticated)/dashboard/UserLevelModule.tsx
+++ app/(authenticated)/dashboard/UserLevelModule.tsx
@@ -8,7 +8,7 @@ import React from "react";
 export default function UserLevelModule() {
   const { currentAccount } = useAuth();
   return (
-    <DashboardModule className="col-span-4 flex flex-col gap-4">
+    <DashboardModule className="flex flex-col gap-4">
       <DashboardModule.Title className="flex justify-between items-center">
         <span>Current Level</span>{" "}
         <span className="bg-secondPrimary text-secondPrimary-foreground w-10 h-10 flex items-center justify-center rounded-full">
--- app/(authenticated)/dashboard/page.tsx
+++ app/(authenticated)/dashboard/page.tsx
@@ -1,11 +1,12 @@
 "use client";
 import DashboardModule from "@/components/DashboardModule";
 import Header from "@/components/Header";
-import LevelBar from "@/components/LevelBar";
 import { Button, buttonVariants } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 import Link from "next/link";
 import UserLevelModule from "./UserLevelModule";
+import hero from "@/assets/images/session-hero.svg";
+import Image from "next/image";
 
 function Dashboard() {
   return (
@@ -14,17 +15,34 @@ function Dashboard() {
       <main className="p-4 container mx-auto">
         <div className="grid grid-cols-12 gap-4">
           <DashboardModule className="col-span-8">
-            <DashboardModule.Title>Title</DashboardModule.Title>
+            <DashboardModule.Title>Sessions</DashboardModule.Title>
             <div>
-              <Link
-                href="/quiz-session"
-                className={cn(buttonVariants(), "font-bold")}
-              >
-                Start a Session
-              </Link>
+              <p>Increase your Level and get better at English.</p>
+              <div className="flex flex-col items-center gap-4 mt-8">
+                <Image
+                  src={hero}
+                  width={300}
+                  height={400}
+                  alt="hero"
+                  className="w-72"
+                />
+                <Link
+                  href="/quiz-session"
+                  className={cn(buttonVariants(), "font-bold")}
+                >
+                  Start a Session
+                </Link>
+              </div>
             </div>
           </DashboardModule>
-          <UserLevelModule />
+          <div className="col-span-4 flex flex-col gap-4">
+            <UserLevelModule />
+
+            <DashboardModule>
+              <DashboardModule.Title>Friends</DashboardModule.Title>
+              <div>Your friends</div>
+            </DashboardModule>
+          </div>
         </div>
       </main>
     </div>
